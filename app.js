ad97141390919b0d3cae89c6d0b2734a5e615826
Zacxxx <zacandroll@gmail.com>
2024-06-19 12:29:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Configure CesiumJS access token
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2M2MxMjY1MC1jYTg4LTQzNTUtOGU1NC0zZjhlZmY4MGFkMjMiLCJpZCI6MjIzMTU4LCJpYXQiOjE3MTg3ODY1NTN9.ARLbHXa-2d2VYQ2ISg7iinNd8h6x1b0lkKjxZ-R9WyY_TOKEN';
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2M2MxMjY1MC1jYTg4LTQzNTUtOGU1NC0zZjhlZmY4MGFkMjMiLCJpZCI6MjIzMTU4LCJpYXQiOjE3MTg3ODY1NTN9.ARLbHXa-2d2VYQ2ISg7iinNd8h6x1b0lkKjxZ-R9WyY';
 
 // Initialize the Cesium Viewer
 const viewer = new Cesium.Viewer('cesiumContainer', {
@@ -23,7 +23,7 @@ viewer.scene.skyAtmosphere.show = true;
 // Add a rotation animation
 const spinRate = 1.0; // radians per second
 viewer.clock.onTick.addEventListener(function (clock) {
-    const delta = clock.getDeltaTime() / 1000;
+    const delta = viewer.clock.tickDuration;
     viewer.scene.camera.rotate(Cesium.Cartesian3.UNIT_Z, -spinRate * delta);
 });
 
@@ -35,4 +35,10 @@ const redPin = viewer.entities.add({
         image: 'https://img.icons8.com/color/48/000000/marker.png',
         verticalOrigin: Cesium.VerticalOrigin.BOTTOM
     }
+});
+
+// Add terrain
+viewer.terrainProvider = Cesium.createWorldTerrain({
+    requestVertexNormals: true,
+    requestWaterMask: true
 });
